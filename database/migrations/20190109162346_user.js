e4fad4001e75855464916d5b754752b574f9740f
MichaelBoegner <cmikeb@gmail.com>
2019-01-09 16:34:46 -0500

    Spun up Server, Knexfile, Index.js, and made knex migrations for family, users and location

---
+++ database/migrations/20190109162346_user.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('user', user => {
+        user
+          .increments()
+          .notNullable()
+
+        user
+          .string('userName', 128)
+          .notNullable()
+
+        user
+          .string('phone', 128)
+          .notNullable()
+
+        user
+          .string('email', 128)
+          .notNullable()
+
+        user
+          .boolean('isAdmin')
+          .notNullable()
+
+        user
+          .integer('familyID')
+          .references('family.id')
+          .notNullable()
+        });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('user');
+};
