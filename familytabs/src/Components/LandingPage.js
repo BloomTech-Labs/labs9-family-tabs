6fa2d2e8c82474f9b0640085a6e8da8f0b27fbb0
MichaelBoegner <cmikeb@gmail.com>
2019-01-14 13:11:36 -0500

    added front end stripe code and pushing for sania to add token gen

--- familytabs/src/Components/LandingPage.js
+++ familytabs/src/Components/LandingPage.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'; 
-
+import Checkout from './Checkout';
 
 class LandingPage extends Component {
+
+
+  componentDidMount(){
+
+}
+  
     render() {
       return (
-        <div className="LandingPage">
-            <h1>Family Tabs Landing Page</h1>
-            <button>BUY NOW!!!!</button>
-            <Link to='/home/tabs'>Log-In</Link>
+        <div>
+          <div className="LandingPage">
+              <h1>Family Tabs Landing Page</h1>
+              <button>BUY NOW!!!!</button>
+              <Link to='/home/tabs'>Log-In</Link>
+          </div>
+          <div>
+            <p className="App-intro">
+            <Checkout
+              name={'The Road to learn React'}
+              description={'Only the Book'}
+              amount={1}
+            />
+          </p>
+          </div>
         </div>
-      );
+      )
     }
   }
   
