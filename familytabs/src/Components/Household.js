38c972ec9ea8351b7550689b3d6e1d7fa87a779e
Johnathan Justinn <johnathanjustinn@Johnathans-iMac.local>
2019-01-15 17:03:39 -0500

    Code added to household ready to display family data

--- familytabs/src/Components/Household.js
+++ familytabs/src/Components/Household.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Household extends Component {
+
+  constructor() {
+    super();
+    this.state = { familydata: [] };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/familymembers/1')
+    .then(familydata => {
+      console.log(familydata);
+      this.setState({familydata: familydata.data});
+      console.log(this.state); 
+    })
+    .catch(err => {
+      console.log(err); 
+    });
+  
+}
+
+
   render() {
     return (
       <div>
         <h1>I am the Household page</h1>
+        <p>{this.state.familydata}</p>
       </div>
     )
   }
 }
+
+
