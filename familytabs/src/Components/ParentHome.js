eed911763c10235119830779d740021b762e7bb6
Sania Parekh <sparekh@Sanias-MacBook.local>
2019-01-16 13:16:04 -0600

    added calendar to family tabs

--- familytabs/src/Components/ParentHome.js
+++ familytabs/src/Components/ParentHome.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Calendar from './Calendar'
 
 class ParentHome extends Component {
     render() {
       return (
         <div className="ParentHomepage">
             <h1>Parent Homepage</h1>
+            <Calendar />
         </div>
       );
     }
