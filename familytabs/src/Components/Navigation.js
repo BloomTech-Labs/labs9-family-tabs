461f6e1e21dfe50c310f6e247dd10a5dd97f1684
Johnathan Justinn <johnathanjustinn@Johnathans-iMac.local>
2019-01-16 14:12:09 -0500

    Added basic nav stlying with border box fix

--- familytabs/src/Components/Navigation.js
+++ familytabs/src/Components/Navigation.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Navigation.css';
 import {Link} from 'react-router-dom'
+import styled from "styled-components";
+
+const NavBarStyling = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 220px;
+    height: 100vh;
+    background-color: #D3D3D3;
+    border: 1px solid rgb(122, 122, 122);
+    border-radius: 0.1rem;
+    position: fixed;
+    top: 0; 
+`;
 
 const Navigation = props => {
     return (
-      <div className='nav_container'>
+      <NavBarStyling>
        <h1>Family Tabs</h1>
        <div className='nav_buttons_container'>
        <Link to='/home/tabs' className="nav_button">Family Tabs</Link>
@@ -13,9 +27,8 @@ const Navigation = props => {
        <Link to='/home/household' className="nav_button">Household</Link>
        <Link to='/home/billing' className="nav_button">Billing</Link>
        <button onClick={props.auth.logout}>Log out</button>
-       
        </div>
-      </div>
+       </NavBarStyling>
     )
   }
   
