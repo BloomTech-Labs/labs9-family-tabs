5905eed625d8de0a181968d1d498f58e51879a67
theryanclausen <theryanclausen@gmail.com>
2019-01-24 18:34:47 -0700

    location eventtypes from calendar

--- familytabs/src/Components/AddEvent.js
+++ familytabs/src/Components/AddEvent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DatePicker from "react-datepicker";
 import styled from "styled-components";
+import CreatableAdvanced from './CreatableAdvanced'
 import "react-datepicker/dist/react-datepicker.css";
 
 const StyledFormWrapper = styled.div`
@@ -23,66 +24,162 @@ const StyledFormWrapper = styled.div`
     flex-direction: column;
   }
 `;
-export default function AddEvent(props) {
-  const {eventEnd, eventStart, locations, locationID,eventTypes,eventType_id, scheduledEvent_name} = props.state
-  console.log(eventTypes)
-  return (
-    <StyledFormWrapper>
-      <form onSubmit={props.addEventHandler}>
-        <input
-          type="text"
-          name="scheduledEvent_name"
-          onChange={props.inputHandler}
-          placeholder="Event Title"
-          value={scheduledEvent_name}
-        />
 
-        <DatePicker
-          name="eventStart"
-          dateFormat="MMMM d, yyyy h:mm a"
-          selected={eventStart}
-          selectsStart
-          showTimeSelect
-          placeholderText="Event start"
-          value={eventStart}
-          startDate={eventStart}
-          endDate={eventEnd}
-          onChange={props.setStart}
-          timeFormat="HH:mm"
-          popperClassName="popper"
-          popperPlacement="left-start"
-        />
+class AddEvent extends React.Component {
+  state = {
+    scheduledEvent_name: "",
+    eventStart: null,
+    eventEnd: null,
+    eventTypeID: null,
+    locationID: null
+  };
 
-        <DatePicker
-          name="eventEnd"
-          value={eventEnd}
-          selected={eventEnd}
-          selectsEnd
-          showTimeSelect
-          placeholderText="Event end"
-          popperPlacement="right-start"
-          popperClassName="popper"
-          timeFormat="HH:mm"
-          dateFormat="MMMM d, yyyy h:mm a"
-          startDate={eventStart}
-          endDate={eventEnd}
-          onChange={props.setEnd}
-        />
+  setStart = e => {
+    this.setState({ eventStart: e });
+  };
 
-<select onChange={props.inputHandler}  name="Location">
-{locations.map(x => <option value={x.id} key={x.id}>{x.location_name}</option>)}
-{/* <option value='new' >New Location</option> */}
-</select>
+  setEventID = id => {
+    this.setState({ eventTypeID: id });
+  };
 
-<select name="Event Type" onChange={props.inputHandler} >
+  setLocationID = id => {
+    this.setState({ locationID: id });
+  };
 
-{eventTypes.map(x => <option value={x.id} key={x.id}>{x.eventType_name}</option>)}
-{/* <option value='new' >New Event Type</option> */}
-</select>
+  setEnd = e => {
+    this.setState({ eventEnd: e });
+  };
 
-        <button>Add Event</button>
-        <button onClick={props.toggleForm}>Exit</button>
-      </form>
-    </StyledFormWrapper>
-  );
+  inputHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addEventHandler = async e => {
+    e.preventDefault();
+    const {
+      scheduledEvent_name,
+      eventStart,
+      eventEnd,
+      eventTypeID,
+      locationID
+    } = this.state;
+    if (
+      !scheduledEvent_name ||
+      !eventStart ||
+      !eventEnd
+      || !eventTypeID ||!locationID
+       
+    ) {
+      return;
+    }
+    let newEvent = {
+      scheduledEvent_name,
+      eventStart,
+      eventEnd,
+      eventTypeID,
+      locationID,
+      familyID: this.props.familyID,
+      createdByAdmin: this.props.isAdmin ? 1 : 0
+    };
+    await this.props.addToCalendar(newEvent)
+    this.setState({
+      scheduledEvent_name: "",
+      eventStart: null,
+      eventEnd: null,
+      eventTypeID: null,
+      locationID: null
+    });
+  };
+ 
+  render() {
+    const { locations, eventTypes } = this.props.state;
+    const {
+      scheduledEvent_name,
+      eventTypeID,
+      eventStart,
+      locationID,
+      eventEnd
+    } = this.state;
+    return (
+      <StyledFormWrapper>
+        <form onSubmit={this.addEventHandler}>
+          <input
+            type="text"
+            name="scheduledEvent_name"
+            onChange={this.inputHandler}
+            placeholder="Event Title"
+            value={scheduledEvent_name}
+          />
+
+          <DatePicker
+            name="eventStart"
+            dateFormat="yyyyMMdd h:mm"
+            selected={eventStart}
+            selectsStart
+            showTimeSelect
+            placeholderText="Event start"
+            value={eventStart}
+            startDate={eventStart}
+            endDate={eventEnd}
+            onChange={this.setStart}
+            timeFormat="HH:mm"
+            popperClassName="popper"
+            popperPlacement="left-start"
+          />
+
+          <DatePicker
+            name="eventEnd"
+            value={eventEnd}
+            selected={eventEnd}
+            selectsEnd
+            showTimeSelect
+            placeholderText="Event end"
+            popperPlacement="right-start"
+            popperClassName="popper"
+            timeFormat="HH:mm"
+            dateFormat="yyyyMMdd h:mm"
+            startDate={eventStart}
+            endDate={eventEnd}
+            onChange={this.setEnd}
+          />
+          <CreatableAdvanced
+          name='eventType'
+          options = {eventTypes}
+          value = {eventTypeID}
+          setValue= {this.setEventID}
+          addOption={this.props.addOption}
+          familyID={this.props.familyID}
+          ></CreatableAdvanced>
+          <CreatableAdvanced
+          name='location'
+          options = {locations}
+          value = {locationID}
+          setValue= {this.setLocationID}
+          addOption={this.props.addOption}
+          familyID={this.props.familyID}
+          ></CreatableAdvanced>
+
+          {/* <select onChange={this.inputHandler} name="Location">
+            {locations.map(x => (
+              <option value={x.id} key={x.id}>
+                {x.location_name}
+              </option>
+            ))}
+          </select>
+
+          <select name="Event Type" onChange={this.props.inputHandler}>
+            {eventTypes.map(x => (
+              <option value={x.id} key={x.id}>
+                {x.eventType_name}
+              </option>
+            ))}
+          </select> */}
+
+          <button type='submit'>Add Event</button>
+          <button onClick={this.props.toggleForm}>Exit</button>
+        </form>
+      </StyledFormWrapper>
+    );
+  }
 }
+export default AddEvent;
