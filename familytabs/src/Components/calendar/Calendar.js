d5141f3d913773ce8502d590e763a6bf49d4a54a
MichaelBoegner <cmikeb@gmail.com>
2019-01-29 19:49:06 -0500

    Formated calendar to wireframe spec for desktop and mobile

--- familytabs/src/Components/calendar/Calendar.js
+++ familytabs/src/Components/calendar/Calendar.js
@@ -5,23 +5,75 @@ import styled from "styled-components";
 import axios from "axios";
 import Select from "react-select";
 import AddEvent from "./AddEvent";
+import {Button, Colors} from "@blueprintjs/core";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = Calendar.momentLocalizer(moment);
+
+
+const MainStyled = styled.div`
+  display: flex;
+  padding: 0 150px 0 150px;
+
+
+  @media (min-width: 320px) and (max-width: 481px) {
+    display: flex;
+    flex-direction: column-reverse;
+    padding: 0;
+  }
+`;
+
+const LeftSide = styled.div`
+  width: 85%;
+  
+  @media (min-width: 320px) and (max-width: 481px) {
+   width: 100%;
+  }
+`;
+
+const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 15%;
+  margin: 75px 0 75px 15px;
+
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+    margin: 95px 0 75px 15px;
+  }
+
+  @media (min-width: 320px) and (max-width: 481px) {
+    width: 100%;
+    margin: 0 0 25px 0;
+  }
+`;
+
+const SelectStyled = styled(Select) `
+  margin: 50px 15px 0 15px;
+
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+    width: 200px;
+  }
+  
+  @media (min-width: 320px) and (max-width: 481px) {
+    margin: 0 25px 0 25px;
+  }
+
+`;
+
 const StyledCalendar = styled(Calendar)`
   /* play with the inspector in the browser to see what things are called and how best to interact with them. Have fun */
   .rbc-month-view {
-    border-radius: 21px;
-    overflow: hidden;
-    height: 800px;
-    border-color: red;
+    height: 90vh;
   }
+
   .rbc-day-bg {
     background: white;
   }
+
   .rbc-row-content{
     z-index:0;
   }
+
   .rbc-date-cell {
     display: flex;
     justify-content: center;
@@ -29,15 +81,66 @@ const StyledCalendar = styled(Calendar)`
     font-size: 12px;
     padding-top: 5px;
   }
+
   .rbc-toolbar-label {
     font-family: "Lato", sans-serif;
+    color: white;
+    font-size: 24px;
+    margin: 15px;
   }
+
   .rbc-header {
     font-family: "Lato", sans-serif;
     padding: 15px;
     font-weight: 300;
+    color: white;
+  }
+
+  .rbc-btn-group button {
+    color: white;
+  }
+
+  .rbc-btn-group .rbc-active {
+    color: #242943;
+  }
+
+  .rbc-time-header {
+    background-color: #242943;
+  }
+
+  .rbc-time-header .rbc-today {
+    color: #242943;
+    font-weight: 550;
+  } 
+
+  .rbc-time-view {
+    background-color: white;
+  }
+
+  .rbc-calendar .rbc-agenda-view {
+    color: white;
+    margin: 10px;
+  }
+
+  .rbc-agenda-view .rbc-agenda-empty {
+    color: white;
+    margin: 10px;
+  }
+
+  .rbc-header {
+      display: flex; 
+      justify-content: center;
+    }
+
+  @media (min-width: 320px) and (max-width: 481px) {
+    .rbc-month-view {
+      height: 80vh;
+    }
+
+
   }
 `;
+
 const Event = ({ event }) => {
   return (
     <span>
@@ -46,6 +149,7 @@ const Event = ({ event }) => {
     </span>
   );
 };
+
 class CalendarComponent extends Component {
   constructor(props) {
     super(props);
@@ -67,7 +171,6 @@ class CalendarComponent extends Component {
       .map(option => {
         return { value: option.id, label: option.userName };
       })
-      
 
   onInputChange = (inputValue, { action }) => {
     this.setState({ participants: inputValue });
@@ -156,50 +259,67 @@ class CalendarComponent extends Component {
     }
   };
 
+  
   render() {
     let events;
     if (!this.state.events.length) {
       events = this.mapToCalendar(this.props.familyEvents);
     }
     return (
-      <div>
-        <div className="CalendarComponent">
-          <Select
-            placeholder="Filter by Family Members"
-            name="participants"
-            defaultValue={this.state.participants}
-            isMulti
-            options={this.participantToOptions(this.props.family)}
-            value={this.state.participants}
-            onChange={this.onInputChange}
-          />
-          <StyledCalendar
-            day={1}
-            localizer={localizer}
-            defaultDate={new Date()}
-            defaultView="month"
-            events={this.state.events.length ? this.state.events : events}
-            components={{
-              event: Event
-            }}
-          />
-        </div>
-        {this.state.showForm ? (
-          <AddEvent
-            toggleForm={this.toggleForm}
-            addToCalendar={this.addToCalendar}
-            state={this.state}
-            addOption={this.addOption}
-            familyID={this.props.familyID}
-            isAdmin={this.props.profile.isAdmin}
-            family={this.props.family}
-            loadGlobal={this.props.loadState}
-            history={this.props.history}
-          />
-        ) : (
-          <button onClick={this.toggleForm}>New Event</button>
-        )}
-      </div>
+      <MainStyled>
+        <LeftSide>
+           <StyledCalendar
+              day={1}
+              localizer={localizer}
+              defaultDate={new Date()}
+              defaultView="month"
+              events={this.state.events.length ? this.state.events : events}
+              components={{
+                event: Event
+              }}
+            />
+        </LeftSide>
+
+        <RightSide>
+            <div>
+              {this.state.showForm ? (
+                <AddEvent
+                  toggleForm={this.toggleForm}
+                  addToCalendar={this.addToCalendar}
+                  state={this.state}
+                  addOption={this.addOption}
+                  familyID={this.props.familyID}
+                  isAdmin={this.props.profile.isAdmin}
+                  family={this.props.family}
+                  loadGlobal={this.props.loadState}
+                  history={this.props.history}
+                />
+              ) : (
+                <Button 
+                  onmou
+                  onClick={this.toggleForm}
+                  style={{color: Colors.WHITE, 
+                          background: "#242943", 
+                          border: "1px solid #ffffff", 
+                          width:"150px", 
+                          height: "50px",
+                          margin: "15px",}}
+                  text="NEW EVENT">
+                </Button>
+              )}
+            </div>
+
+          <SelectStyled
+                placeholder="Family Members"
+                name="participants"
+                defaultValue={this.state.participants}
+                isMulti
+                options={this.participantToOptions(this.props.family)}
+                value={this.state.participants}
+                onChange={this.onInputChange}
+              />
+          </RightSide>
+      </MainStyled>
     );
   }
 }
