c37116d266a158f4811762d706a332bd370261e9
Johnathan Justinn <johnathanjustinn@Johnathans-iMac.local>
2019-01-30 14:48:40 -0500

    Refactored Navbar to include hover and styling changes

--- familytabs/src/Components/Navigation.js
+++ familytabs/src/Components/Navigation.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import styled from "styled-components";
-import {AnchorButton, Button, Colors} from "@blueprintjs/core";
 import FTLogo4 from './images/FT_Logo_4.jpg';
 
+
 const ImageStyles = styled.img `
-    width: 25%;
-    height: 10%;
+    width: 100%;
+    height: 100%;
     border-radius: 25%;
+`;
+
+const LogoStyles = styled.a`
+    width: 30%;
+    height: 30%;
+    margin-bottom: 10px;
+    :hover {
+        cursor: pointer;
+    }
 `;
 
 const ImgWrapper = styled.div`
@@ -21,39 +30,72 @@ const NavBarStyling = styled.div`
     font-family: 'Lato', sans-serif;
     display: flex;
     flex-direction: column;
+    align-content: center;
     width: 280px;
     background-color: #242943;
     border: 1px solid rgb(122, 122, 122);
     border-radius: 0.1rem;
-    /* position: fixed; */
     top: 0;
     height: 100%;
 `;
 
 
-const NavButtonsContainer = styled.div`
+const NavLinksContainer = styled.div`
     margin: 0.5rem 0 0 0;
+    display: flex;
+    flex-wrap: wrap;
     flex-direction: column;
+    justify-content: center;
+`;
+
+const NavLink = styled(Link) `
+    color: white;
+    background: #242943;
+    display: flex;
+    justify-content: center;
+    width: 100%; 
+    height: 50px;
+    margin-bottom: 6.5px;
+    :hover {
+    text-decoration: none;
+    color: #3985ac;
+}
+`;
+
+const LogoutButton = styled.button`
+    color: white;
+    background: #242943;
+    display: flex;
+    justify-content: center;
+    border: none;
+    width: 100%; 
+    height: 50px;
+    margin-bottom: 6.5px;
+    :hover {
+      cursor: pointer;
+      color: #3985ac;
+}
 `;
 
 
 class Navigation extends Component {
 
-
     render(){
     return (
       <NavBarStyling>
        <ImgWrapper>
+       <LogoStyles href='/home/tabs'>
        <ImageStyles src={FTLogo4} alt="FTLogo3"></ImageStyles>
+       </LogoStyles>
        </ImgWrapper>
-        <NavButtonsContainer>
-            <AnchorButton minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943",  width:"100%", height: "50px", marginBottom: "6.5px"}} href="/home/tabs" text="Family Tabs" />
-            <AnchorButton minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943",  width:"100%", height: "50px", marginBottom: "6.5px"}} href="/home/notifications" text="Notifications" />
-            <AnchorButton minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943",  width:"100%", height: "50px", marginBottom: "6.5px"}} href="/home/settings" text="Settings" />
-            <AnchorButton minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943",  width:"100%", height: "50px", marginBottom: "6.5px"}} href="/home/household" text="Household" />
-            <AnchorButton minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943",  width:"100%", height: "50px", marginBottom: "6.5px"}} href="/home/billing" text="Billing" />
-        </NavButtonsContainer>
-       <Button minimal={true} style={{ color: Colors.WHITE, colorbackground: "#242943", width:"100%", height: "50px"}} onClick={this.props.auth.logout}>Log out</Button>
+        <NavLinksContainer>
+            <NavLink to='/home/tabs'>Family Tabs</NavLink>
+            <NavLink to='/home/notifications'>Notifications</NavLink>
+            <NavLink to='/home/settings'>Settings</NavLink>
+            <NavLink to='/home/household'>Household</NavLink>
+            <NavLink to='/home/billing'>Billing</NavLink>
+        </NavLinksContainer>
+       <LogoutButton onClick={this.props.auth.logout}>Log out</LogoutButton>
         </NavBarStyling>
     )
   }
@@ -64,63 +106,3 @@ class Navigation extends Component {
 
 
 
-// import React from 'react';
-// import styled from "styled-components";
- 
-
-// const NavBarStyling = styled.div`
-//     font-family: sans-serif;
-// `;
-// const StyledNav = styled(NavBarStyling)`
-//     h1 {
-//      text-align: center;
-//     }
-
-//    .NavButtonsContainer a {
-//     display: block;
-//     padding: .7rem 15px;
-//     color: inherit;
-//     text-decoration: none;
-
-//     .NavButtonsContainer a:hover {
-//     color: inherit;
-//     text-decoration: none;
-//     background-color: #eee;
-// }
-
-//     .NavButtonsContainer a:focus {
-//     text-decoration: none;
-// }
-
-//     .NavButtonsContainer a:visited {
-//     color: inherit;
-// }
-// }
-// `;
-
-
-// class Navigation extends React.Component {
-
-//     render(){
-       
-//     return (
-//       <NavBarStyling>
-//       <StyledNav>
-//        <h1>Family Tabs</h1>
-//         <div classname = 'NavButtonsContainer'>
-//             <a href='/home/tabs'>Family Tabs</a>
-//             <a href='/home/notifications'>Notifications</a>
-//             <a href='/home/settings'>Account Settings</a>
-//             <a href='/home/household'>Household</a>
-//             <a href='/home/billing' className="nav_button">Billing</a>
-
-//             {/* <Buttons onClick={this.props.auth.logout}>Log out</Buttons> */}
-//         </div>
-//         </StyledNav>
-//        </NavBarStyling>
-    
-//     )
-//   }
-//   }
-//   export default Navigation;
-  
