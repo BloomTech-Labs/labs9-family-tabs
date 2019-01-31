4f79743d586d04968c1ff449b1da3ef46f510649
Johnathan Justinn <johnathanjustinn@Johnathans-iMac.local>
2019-01-31 15:19:15 -0500

    Getting Start Page Styled

--- familytabs/src/Components/SignUp.js
+++ familytabs/src/Components/SignUp.js
@@ -1,16 +1,77 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'; 
 import styled from "styled-components";
+import { Card } from "@blueprintjs/core";
 
 
 
 const  SignUpTop = styled.div`
-  border: 2px solid red; 
   max-width: 100%;
   max-height: 100%;
-  /* display: flex; 
-  align-items: center; 
-  justify-content: center; */
+`;
+
+const Title = styled.h1`
+    margin: 0 0 0 0;
+    display: flex;
+    justify-content: center;
+    color: #ffffff;
+    font-size: 50px;
+    padding:0;
+    font-weight: 700;
+`;
+
+const BottomBorder = styled.div`
+    border-bottom: 2px solid #D4B36E;
+    height: 20px;
+    width: 100%;
+`;
+
+const Content = styled.p `
+    text-align: left;
+    color: #ffffff;
+    font-size: 18px;
+    line-height: 1.5;
+    padding: 20px 20px 0 20px;
+    margin: 10px 0 30px 0;
+`;
+
+const SubTitle = styled.h3`
+    margin: 0 0 10px 0;
+    display: flex;
+    justify-content: center;
+    color: #ffffff;
+    font-size: 25px;
+    padding:0;
+    font-weight: 200;
+`;
+
+const SubContent = styled.p `
+    text-align: center;
+    color: #ffffff;
+    font-size: 18px;
+    line-height: 1.5;
+    padding: 20px 20px 0 20px;
+    margin: 5px 0 10px 0;
+`;
+
+const CardContainer = styled.div`
+  border: 2px #242943;
+  display: flex;
+  align-content: flex-start;
+  justify-content: space-around;
+`;
+
+const StyledCard = styled(Card)`
+  border: 2px solid #D4B36E;
+  background-color: #242943;
+  margin: 15px 15px 15px 15px;
+  border-radius: 25px;
+`;
+
+const StyledLink = styled(Link)`
+  :hover {
+    text-decoration: none;
+  }
 `;
 
 
@@ -21,18 +82,23 @@ class SignUp extends Component {
       return (
         <div>
           <SignUpTop>
-              <h1>Join Family Tabs today! Choose from our standard or premium account options.</h1>
-              <p>Family Tabs free accounts automatically offers a single parent account with up to two children. For only 9.99 a month the Premium account includes three admins and and unlimited amount of children accounts. Your information is always safe with us, and will never be sold to third parties under any circumstances. With Family Tabs we make it easy to keep tabs on the whole family!</p>
-              <h3>Standard Family Tabs Option</h3>
-              <p>Enjoy two Admin accounts and two child accounts with our free standard option</p>
-              <Link to="/verify">Sign Up for Family Tabs!</Link>
-              <h3>Premium Family Tabs Option</h3>
-              <p>Have a larger brood? Add an extra Admin account, and unlimited child accounts with our Premium option for just $9.99 a month!</p>
-              <Link to="/verify">Sign Up for Family Tabs Premium!</Link>
-
-            
-            
-              
+              <Title>Join Family Tabs!</Title>
+              <BottomBorder></BottomBorder>
+              <Content>Family Tabs free accounts automatically offers a single parent account with up to two children. For only 9.99 a month the Premium account includes three admins and 
+                unlimited amount of children accounts. Your information is always safe with us, and will never be sold to third parties under any circumstances. With Family Tabs we make it 
+                easy to keep tabs on the whole family!</Content>
+              <CardContainer>
+              <StyledCard>
+              <SubTitle>Standard Family Tabs Option</SubTitle>
+              <SubContent>Enjoy two Admin accounts and two child accounts with our free standard option</SubContent>
+              <StyledLink to="/verify">Sign Up for Family Tabs!</StyledLink>
+              </StyledCard>
+              <StyledCard>
+              <SubTitle>Premium Family Tabs Option</SubTitle>
+              <SubContent>Have a larger brood? Add an extra Admin account, and unlimited child accounts with our Premium option for just $9.99 a month!</SubContent>
+              <StyledLink to="/verify">Sign Up for Family Tabs Premium!</StyledLink>
+              </StyledCard>
+              </CardContainer>
           </SignUpTop>
         </div>
       )
@@ -40,3 +106,71 @@ class SignUp extends Component {
   }
   
   export default SignUp;
+
+
+
+
+
+// const BottomBorder = styled.div`
+//   border-bottom: 2px solid #D4B36E;
+//   height: 20px;
+//   width: 100%;
+//   margin: 0px 0px 50px 0px;
+ 
+
+// `;
+// const Title = styled.p `
+//    display: flex;
+ 
+//     color: #ffffff;
+//     font-size: 16px;
+//     padding-left: 5px;
+//     color: #3985ac;
+//     width: 20%;
+//     margin: 0px 0px 10px 125px; 
+  
+
+  
+
+// `;
+
+// const BillingWrapper=styled.div `
+
+
+
+// `;
+
+// const BillingTitle = styled.div `
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+
+
+
+// `;
+
+// const CardContainer = styled.div `
+ 
+//     display: flex;
+//     justify-content: center;
+// `;
+
+
+
+// export default class Billing extends Component {
+//   render() {
+//     return (
+//     <BillingWrapper>
+//         <BillingTitle>
+//         <Title>upgrade to a premium account</Title>
+//         <Title>Billing</Title>
+//         <BottomBorder></BottomBorder>
+//         </BillingTitle>
+//       <CardContainer>
+//       <BillingCard />
+
+//       </CardContainer>
+//       </BillingWrapper>
+//     )
+//   }
+// }
