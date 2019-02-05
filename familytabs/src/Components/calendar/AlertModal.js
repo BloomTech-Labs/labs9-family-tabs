50d7d378e753348638df44120f586bb90ff7738c
Johnathan Justinn <johnathanjustinn@Johnathans-iMac.local>
2019-02-05 18:06:33 -0500

    Added Alert Modal Component

---
+++ familytabs/src/Components/calendar/AlertModal.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { StyledFormWrapper } from "../styled/components";
+
+export default class AlertModal extends Component {
+
+
+  render() {
+    return (
+        <StyledFormWrapper>
+            <h2>Success!</h2>
+            <button onClick={this.props.addEventHandler}>Exit</button>
+        </StyledFormWrapper>
+    )
+  }
+
+}
+
