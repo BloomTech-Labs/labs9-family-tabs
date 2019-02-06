9035f74e5fe6bc422ca0e01bc678162e216a1917
theryanclausen <theryanclausen@gmail.com>
2019-02-06 00:40:58 -0700

    notification modal fix

--- familytabs/src/Components/notifications/EventCard.js
+++ familytabs/src/Components/notifications/EventCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import moment from "moment";
 import EventEdit from "../event-edit/EventEdit";
+import NotificationsModal from "./NotificationsModal";
 
 // const Card = styled.div`
 //   border: 2px solid lightgrey;
@@ -43,10 +44,10 @@ const Card = styled.div`
   flex-direction: column;
   align-items: flex-start;
   line-height: 2;
-  background: #68659E;
+  background: #68659e;
   margin: 28px auto;
-  @media (max-width:450px){
-    width:100%;
+  @media (max-width: 450px) {
+    width: 100%;
   }
 `;
 
@@ -62,51 +63,54 @@ const Header = styled.div`
 `;
 
 const Info = styled.div`
-  width:100%;
-  font-size:15px;
+  width: 100%;
+  font-size: 15px;
   padding: 0 10px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   padding-bottom: 10px;
   color: #ffffff;
-  p{
-    width:100%;
-    text-align:center;
+  p {
+    width: 100%;
+    text-align: center;
   }
 `;
 
-const StyledButton = styled.button `
-    margin: 10px 0px 20px 10px;
-    color: white;
-    background: #68659E;
-    border: 2px solid #ffffff; 
-    /* padding: 15px 50px 15px 50px; */
-    width: 100px; 
-    height: 25px;
-    margin-left: 5px;
-    
-    :hover {
-      border-color: #3985ac;
-      color: #3985ac;
-      cursor: pointer;
-    }
+const StyledButton = styled.button`
+  margin: 10px 0px 20px 10px;
+  color: white;
+  background: #68659e;
+  border: 2px solid #ffffff;
+  /* padding: 15px 50px 15px 50px; */
+  width: 100px;
+  height: 25px;
+  margin-left: 5px;
+
+  :hover {
+    border-color: #3985ac;
+    color: #3985ac;
+    cursor: pointer;
+  }
 `;
 
 const ButtonBox = styled.div`
-width:100%;
-  display:flex;
+  width: 100%;
+  display: flex;
   justify-content: space-evenly;
-`
+`;
 
 class EventCard extends React.Component {
-  state={showForm:false}
-    toggleForm = () => {
-      this.setState({ showForm: !this.state.showForm });
-    };
+  state = { showForm: false, showModal: false };
+  toggleForm = () => {
+    this.setState({ showForm: !this.state.showForm });
+  };
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
 
   render() {
-
     return (
       <Card>
         <Header>EVENT: {this.props.eventData.scheduledEvent_name}</Header>
@@ -134,37 +138,54 @@ class EventCard extends React.Component {
         </Info>
         {/* this ternary checks to see if this event card was created by one of the pending events. It also makes sure that the user is an admin. 
         If both of those tests our passed, the buttons will render. If not, the buttons will not display*/}
-        
-          <ButtonBox>
-            <StyledButton onClick={this.toggleForm}>EDIT</StyledButton>
 
-          {this.props.pending  ? (  <> <StyledButton
-              onClick={this.props.approveClick}
-              id={this.props.eventData.id}
-            >
-              APPROVE
-            </StyledButton>
-            <StyledButton
-              onClick={this.props.declineClick}
-              id={this.props.eventData.id}
-            >
-              DECLINE
-            </StyledButton></>): (
+        <ButtonBox>
+          <StyledButton onClick={this.toggleForm}>EDIT</StyledButton>
+
+          {this.props.pending ? (
+            <>
+              {" "}
+              <StyledButton
+                onClick={this.props.approveClick}
+                id={this.props.eventData.id}
+              >
+                APPROVE
+              </StyledButton>
+              <StyledButton
+                onClick={this.toggleModal}
+                id={this.props.eventData.id}
+              >
+                DECLINE
+              </StyledButton>
+            </>
+          ) : (
+            ""
+          )}
+        </ButtonBox>
+
+        {/* // everything before the colon will load if our tests pass. everything after the colon will load if they don't */}
+        {this.state.showModal ? (
+          <NotificationsModal
+            declineClick={this.props.declineClick}
+            toggleModal={this.toggleModal}
+            id={this.props.eventData.id}
+          />
+        ) : (
           ""
         )}
-          </ButtonBox>
-         
-        {/* // everything before the colon will load if our tests pass. everything after the colon will load if they don't */}
-        
 
-{this.state.showForm ? <EventEdit
-          pendingEditId={this.props.eventData.id}
-          loadGlobal={this.props.loadState}
-          pendingEdit={this.props.eventData}
-          family={this.props.family}
-          toggleForm={this.toggleForm}
-          profile={this.props.profile}
-        /> : ""}
+        {this.state.showForm ? (
+          <EventEdit
+            pendingEditId={this.props.eventData.id}
+            loadGlobal={this.props.loadState}
+            pendingEdit={this.props.eventData}
+            family={this.props.family}
+            toggleForm={this.toggleForm}
+            profile={this.props.profile}
+          />
+        ) : (
+          ""
+        )}
       </Card>
     );
   }
---
+++ familytabs/src/Components/notifications/NotificationsModal.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { StyledFormWrapper } from "../styled/components";
+
+export default class NotificationsModal extends Component {
+  render() {
+    return (
+      <StyledFormWrapper>
+        <form>
+          <h2>Are you sure you want to decline this event request?</h2>
+          <div className="button-box">
+            <button id={this.props.id} onClick={this.props.declineClick}>Decline Request</button>
+            <button onClick={this.props.toggleModal}>Exit</button>
+          </div>
+        </form>
+      </StyledFormWrapper>
+    );
+  }
+}
